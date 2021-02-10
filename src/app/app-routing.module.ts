237a4966d5235b3af72b178a45485fd097ea3eab
twolak88 <twolak88@gmail.com>
2021-02-10 21:53:48 +0100

    Redirecting and Wildcard Routes/cover unknown route links by ** in last position

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -15,7 +16,9 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children:[
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
-  ] }
+  ] },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { UserComponent } from './users/user/user.component';
 import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     UsersComponent,
     UserComponent,
     ServerComponent,
-    EditServerComponent
+    EditServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
