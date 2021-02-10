3039725ee80dac011a1f6c5b7126801066ecb508
twolak88 <twolak88@gmail.com>
2021-02-10 22:57:59 +0100

    Protecting Routes with canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AuthGuard } from './services/auth-guard.service';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 
@@ -16,7 +17,7 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent } //dynamic parameter id
   ] },
-  { path: 'servers', component: ServersComponent, children:[
+  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children:[
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
   ] },
